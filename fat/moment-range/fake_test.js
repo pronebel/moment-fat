27a3b5f09ae1ed9df5f75a99a77e9a0f9b9d5075
luokw <luokw@tpl.cntaiping.com>
2017-05-26 20:28:32 +0800

    test constructor

--- fat/moment-range/fake_test.js
+++ fat/moment-range/fake_test.js
@@ -7,6 +7,9 @@ import expect from 'expect.js';
 import { DateRange,DateRangeCreate } from './fat/moment-range';
 import * as fake  from './fat/fake'
 
+function isDate(date){
+  return date instanceof Date
+}
 
 describe('DateRange', function() {
   const d1 = new Date(Date.UTC(2011, 2, 5));
@@ -20,78 +23,78 @@ describe('DateRange', function() {
   //const sStart = '1996-08-12T00:00:00.000Z';
   const sEnd = '2012-01-01T00:00:00.000Z';
 
-  // describe('constructor', function() {
-  //   it('should allow initialization with date string', function() {
-  //     const dr = moment.range(sStart, sEnd);
-  //
-  //     expect(moment.isMoment(dr.start)).to.be(true);
-  //     expect(moment.isMoment(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with Date object', function() {
-  //     const dr = moment.range(d1, d2);
-  //
-  //     expect(moment.isMoment(dr.start)).to.be(true);
-  //     expect(moment.isMoment(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with Moment object', function() {
-  //     const dr = moment.range(m1, m2);
-  //
-  //     expect(moment.isMoment(dr.start)).to.be(true);
-  //     expect(moment.isMoment(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with an ISO 8601 Time Interval string', function() {
-  //     const start = '2015-01-17T09:50:04+00:00';
-  //     const end   = '2015-04-17T08:29:55+00:00';
-  //     const dr = moment.range(start + '/' + end);
-  //
-  //     expect(moment.utc(start).isSame(dr.start)).to.be(true);
-  //     expect(moment.utc(end).isSame(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with an array', function() {
-  //     const dr = moment.range([m1, m2]);
-  //
-  //     expect(m1.isSame(dr.start)).to.be(true);
-  //     expect(m2.isSame(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with open-ended ranges', function() {
-  //     let dr = moment.range(null, m1);
-  //
-  //     expect(moment.isMoment(dr.start)).to.be(true);
-  //
-  //     dr = moment.range(m1, null);
-  //
-  //     expect(moment.isMoment(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization without any arguments', function() {
-  //     const dr = moment.range();
-  //
-  //     expect(moment.isMoment(dr.start)).to.be(true);
-  //     expect(moment.isMoment(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with undefined arguments', function() {
-  //     const dr = moment.range(undefined, undefined);
-  //
-  //     expect(moment.isMoment(dr.start)).to.be(true);
-  //     expect(moment.isMoment(dr.end)).to.be(true);
-  //   });
-  //
-  //   it('should allow initialization with moment interval strings', function() {
-  //     const date = moment('2016-12-12T11:12:18.607');
-  //     const quarterStart = moment('2016-10-01T00:00:00.000');
-  //     const quarterEnd = moment('2016-12-31T23:59:59.999');
-  //     const r = date.range('quarter');
-  //
-  //     expect(r.start.isSame(quarterStart)).to.be(true);
-  //     expect(r.end.isSame(quarterEnd)).to.be(true);
-  //   });
-  // });
+  describe('constructor', function() {
+   /* it('should allow initialization with date string', function() {
+      const dr = moment.range(sStart, sEnd);
+
+      expect(moment.isMoment(dr.start)).to.be(true);
+      expect(moment.isMoment(dr.end)).to.be(true);
+    });*/
+
+    it('should allow initialization with Date object', function() {
+      const dr = DateRangeCreate(d1, d2);
+
+      expect(isDate(dr.start)).to.be(true);
+      expect(isDate(dr.end)).to.be(true);
+    });
+
+   /* it('should allow initialization with Moment object', function() {
+      const dr = moment.range(m1, m2);
+
+      expect(moment.isMoment(dr.start)).to.be(true);
+      expect(moment.isMoment(dr.end)).to.be(true);
+    });*/
+
+    /*it('should allow initialization with an ISO 8601 Time Interval string', function() {
+      const start = '2015-01-17T09:50:04+00:00';
+      const end   = '2015-04-17T08:29:55+00:00';
+      const dr = moment.range(start + '/' + end);
+
+      expect(moment.utc(start).isSame(dr.start)).to.be(true);
+      expect(moment.utc(end).isSame(dr.end)).to.be(true);
+    });*/
+
+    it('should allow initialization with an array', function() {
+      const dr = DateRangeCreate([d1, d2]);
+
+      expect(fake.isSame(d1,dr.start)).to.be(true);
+      expect(fake.isSame(d2,dr.end)).to.be(true);
+    });
+
+    it('should allow initialization with open-ended ranges', function() {
+      let dr = DateRangeCreate(null, d1);
+
+      expect(isDate(dr.start)).to.be(true);
+
+      dr = DateRangeCreate(d1, null);
+
+      expect(isDate(dr.end)).to.be(true);
+    });
+
+    it('should allow initialization without any arguments', function() {
+      const dr = DateRangeCreate();
+
+      expect(isDate(dr.start)).to.be(true);
+      expect(isDate(dr.end)).to.be(true);
+    });
+
+    it('should allow initialization with undefined arguments', function() {
+      const dr = DateRangeCreate(undefined, undefined);
+
+      expect(isDate(dr.start)).to.be(true);
+      expect(isDate(dr.end)).to.be(true);
+    });
+
+   /* it('should allow initialization with moment interval strings', function() {
+      const date = moment('2016-12-12T11:12:18.607');
+      const quarterStart = moment('2016-10-01T00:00:00.000');
+      const quarterEnd = moment('2016-12-31T23:59:59.999');
+      const r = date.range('quarter');
+
+      expect(r.start.isSame(quarterStart)).to.be(true);
+      expect(r.end.isSame(quarterEnd)).to.be(true);
+    });*/
+  });
 
   describe('#adjacent', function() {
     it('should correctly indicate when ranges aren\'t adjacent', function() {
