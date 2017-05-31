3df660ed6520b66822f2e462ebc1cae826ffa4da
luokw <luokw@tpl.cntaiping.com>
2017-05-31 14:29:46 +0800

    拆分出复杂的使用不高的部分

--- fat/moment-range/fat/moment-range.js
+++ fat/moment-range/fat/moment-range.js
@@ -55,69 +55,6 @@ export class DateRange {
         return null;
     }
 
-    by(interval, options = {exclusive: false, step: 1}) {// change
-        const range = this;
-
-        return {
-            [Symbol.iterator]() {
-                const exclusive = options.exclusive || false;
-                const step = options.step || 1;
-                const diff = Math.abs(fake.diff(range.start,range.end, interval)) / step;
-                let iteration = 0;
-
-                return {
-                    next() {
-                        const current = fake.add(new Date(range.start.valueOf()),(iteration * step), interval);
-                        console.log(current);
-                        const done = exclusive
-                            ? !(iteration < diff)
-                            : !(iteration <= diff);
-
-                        iteration++;
-
-                        return {
-                            done,
-                            value: (done ? undefined : current)
-                        };
-                    }
-                };
-            }
-        };
-    }
-
-    byRange(interval, options = {exclusive: false, step: 1}) { //change
-        const range = this;
-        const step = options.step || 1;
-        const diff = this.valueOf() / interval.valueOf() / step;
-        const exclusive = options.exclusive || false;
-        const unit = Math.floor(diff);
-        let iteration = 0;
-
-        return {
-            [Symbol.iterator]() {
-                if (unit === Infinity) {
-                    return {done: true};
-                }
-
-                return {
-                    next() {
-                        const current = new Date(range.start.valueOf() + (interval.valueOf() * iteration * step));
-                        const done = ((unit === diff) && exclusive)
-                            ? !(iteration < unit)
-                            : !(iteration <= unit);
-
-                        iteration++;
-
-                        return {
-                            done,
-                            value: (done ? undefined : current)
-                        };
-                    }
-                };
-            }
-        };
-    }
-
     center() { // change
         const center = this.start.valueOf() + this.diff() / 2;
 
@@ -146,8 +83,9 @@ export class DateRange {
         return (startInRange && endInRange);
     }
     //todo: 考虑diff只管算出相差的毫秒值,至于 毫秒值是多少天,等交给专门的库去处理
-    diff(unit, rounded) { // fake
-        return fake.diff(this.end, this.start, unit, rounded);
+    diff() { // fake
+        //return fake.diff(this.end, this.start, unit, rounded);
+        return Math.floor(this.end.valueOf() - this.start.valueOf());
     }
 
     duration(unit, rounded) { // no change
@@ -192,68 +130,6 @@ export class DateRange {
         }
 
         return intersect;
-    }
-
-    reverseBy(interval, options = {exclusive: false, step: 1}) { // change fake
-        const range = this;
-
-        return {
-            [Symbol.iterator]() {
-                const exclusive = options.exclusive || false;
-                const step = options.step || 1;
-                const diff = Math.abs(fake.diff(range.start,range.end, interval)) / step;
-                let iteration = 0;
-
-                return {
-                    next() {
-                        const current = fake.subtract(new Date(range.end.valueOf()),(iteration * step), interval);
-                        const done = exclusive
-                            ? !(iteration < diff)
-                            : !(iteration <= diff);
-
-                        iteration++;
-
-                        return {
-                            done,
-                            value: (done ? undefined : current)
-                        };
-                    }
-                };
-            }
-        };
-    }
-
-    reverseByRange(interval, options = {exclusive: false, step: 1}) {  // change
-        const range = this;
-        const step = options.step || 1;
-        const diff = this.valueOf() / interval.valueOf() / step;
-        const exclusive = options.exclusive || false;
-        const unit = Math.floor(diff);
-        let iteration = 0;
-
-        return {
-            [Symbol.iterator]() {
-                if (unit === Infinity) {
-                    return {done: true};
-                }
-
-                return {
-                    next() {
-                        const current = new Date(range.end.valueOf() - (interval.valueOf() * iteration * step));
-                        const done = ((unit === diff) && exclusive)
-                            ? !(iteration < unit)
-                            : !(iteration <= unit);
-
-                        iteration++;
-
-                        return {
-                            done,
-                            value: (done ? undefined : current)
-                        };
-                    }
-                };
-            }
-        };
     }
 
     subtract(other) {  // no change
